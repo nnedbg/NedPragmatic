e6424deb8b653dc79a10b52390cce3608b15de56
nedialko.tanev <saik2tt@gmail.com>
2019-05-16 17:52:42 +0300

    My First commit :D

---
+++ Homework1/src/Homework.java
@@ -0,0 +1,20 @@
+public class Homework {
+
+    public static void main(String[] args) {
+        System.out.println("Message for user");
+            int A = 10;
+            int B = 5;
+            double C = 7.4;
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+
+        System.out.println("A = " +  A);
+        System.out.println("B = " +  B);
+        System.out.println("C = " +  C);
+
+
+
+    }
+}
